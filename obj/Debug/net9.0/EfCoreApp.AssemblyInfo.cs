2803bae81b255803401700dbbb5486dc99d6bf7b
RSF090 <rsfurkan09@gmail.com>
2025-05-12 17:00:37 +0300

    Kayıt formu oluşturuldu

--- obj/Debug/net9.0/EfCoreApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/EfCoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EfCoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10e79326a19ba960b6da34b3cf298c055e0b121b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a776aee57a7817a5f8691fdef13cca78fac6ca6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("EfCoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EfCoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
